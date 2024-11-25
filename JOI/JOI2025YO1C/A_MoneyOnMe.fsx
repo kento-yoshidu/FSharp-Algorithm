7425b3d3fcc350d4d7bfe7fcbdd3d797447d3ed4
kento <kento@toriwatari.bird>
2024-11-25 19:41:58 +0900

    💎 JOI2025YO1C A

---
+++ JOI/JOI2025YO1C/A_MoneyOnMe.fsx
@@ -0,0 +1,10 @@
+// https://atcoder.jp/contests/joi2025yo1c/tasks/joi2025_yo1c_a
+
+let run a b =
+  a * 1000 + b * 10000
+
+printfn "%i" (run 7 2)
+//=> 27000
+
+printfn "%i" (run 11 1)
+//=> 21000
