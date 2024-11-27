27e4496987339ea2b2e2562d93313c9999d7a4fe
kento <kento@toriwatari.bird>
2024-11-27 23:40:34 +0900

    🥐 ABC005 A

---
+++ ABC0XX/ABC005/A_Takoyaki.fsx
@@ -0,0 +1,13 @@
+// https://atcoder.jp/contests/abc005/tasks/abc005_1
+
+let run x y =
+    y / x
+
+printfn "%i" (run 4 8)
+//=> 2
+
+printfn "%i" (run 4 7)
+//=> 1
+
+printfn "%i" (run 4 3)
+//=> 0
