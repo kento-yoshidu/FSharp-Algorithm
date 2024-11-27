9cc3b4c16630291944ef9eb32b91428b8f778ab9
kento <kento@toriwatari.bird>
2024-11-27 21:20:39 +0900

    👩 ABC004 A

---
+++ ABC0XX/ABC004/A_Fashion.fsx
@@ -0,0 +1,13 @@
+// https://atcoder.jp/contests/abc004/tasks/abc004_1
+
+let run n =
+    n * 2
+
+printfn "%i" (run 1000)
+//=> 2000
+
+printfn "%i" (run 1000000)
+//=> 2000000
+
+printfn "%i" (run 0)
+//=> 0
