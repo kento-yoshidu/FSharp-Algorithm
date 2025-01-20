f490cab4b3568add7d7bc6100127de38adc63a02
kento <kento@toriwatari.bird>
2025-01-20 21:24:37 +0900

    🥄 ABC068 A

---
+++ AtCoder/ABC0XX/ABC068/A.fsx
@@ -0,0 +1,13 @@
+// https://atcoder.jp/contests/abc068/tasks/abc068_a
+
+let run n =
+    "ABC" + string n
+
+printfn "%s" (run 100)
+//=> ABC100
+
+printfn "%s" (run 425)
+//=> ABC425
+
+printfn "%s" (run 999)
+//=> ABC999
