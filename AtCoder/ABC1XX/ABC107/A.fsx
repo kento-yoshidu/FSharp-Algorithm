082dad4a63e4ecb80387083a206a387d84be578d
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-09-11 20:13:06 +0900

    ☔ ABC107 A

---
+++ AtCoder/ABC1XX/ABC107/A.fsx
@@ -0,0 +1,13 @@
+// https://atcoder.jp/contests/abc107/tasks/abc107_a
+
+let fn n i =
+    n - i + 1
+
+printfn "%i" (fn 4 2)
+//=> 3
+
+printfn "%i" (fn 1 1)
+//=> 1
+
+printfn "%i" (fn 15 11)
+//=> 5
