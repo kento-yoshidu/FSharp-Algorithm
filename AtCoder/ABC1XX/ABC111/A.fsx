1ccce522e6717c6e56008d41a2a95305a5332209
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-09-12 20:51:34 +0900

    🦅 ABC111 A

---
+++ AtCoder/ABC1XX/ABC111/A.fsx
@@ -0,0 +1,13 @@
+// https://atcoder.jp/contests/abc111/tasks/abc111_a
+
+let fn n =
+    1110 - n
+
+printfn "%i" (fn 119)
+//=> 991
+
+printfn "%i" (fn 999)
+//=> 111
+
+printfn "%i" (fn 191)
+//=> 919
