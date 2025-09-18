d545bcfae4a786712df48f8da15d5325f245cad1
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-09-18 20:25:20 +0900

    🙆 ABC117A

---
+++ AtCoder/ABC1XX/ABC117/A.fsx
@@ -0,0 +1,13 @@
+// https://atcoder.jp/contests/abc117/tasks/abc117_a
+
+let fn (t: float) (x: float) =
+    t / x
+
+printfn "%f" (fn 8 3)
+//=> 2.666667
+
+printfn "%f" (fn 99 1)
+//=> 99.000000
+
+printfn "%f" (fn 1 100)
+//=> 0.010000
