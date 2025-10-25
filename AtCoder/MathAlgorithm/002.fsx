de26606ecd58ebea537e4278f8ee66830182a4bc
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-10-25 14:12:32 +0900

    🛰️ Update

---
+++ AtCoder/MathAlgorithm/002.fsx
@@ -0,0 +1,13 @@
+// https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_b
+
+let fn a1 a2 a3 =
+    a1 + a2 + a3
+
+printfn "%i" (fn 10 20 50)
+//=> 80
+
+printfn "%i" (fn 1 1 1)
+//=> 3
+
+printfn "%i" (fn 100 100 100)
+//=> 300
