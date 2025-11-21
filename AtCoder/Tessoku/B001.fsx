398ee6219c67d401a04c1f6e5a736f4d628ce66b
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-11-21 21:32:30 +0900

    🐖 Update

---
+++ AtCoder/Tessoku/B001.fsx
@@ -0,0 +1,13 @@
+// https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bz
+
+let fn a b =
+    a + b
+
+printfn "%i" (fn 1 2)
+//=> 3
+
+printfn "%i" (fn 77 23)
+//=> 100
+
+printfn "%i" (fn 100 100)
+//=> 200
