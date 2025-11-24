eebad5c2287dbcbb856a50916427a4c495b114ab
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-11-24 20:29:12 +0900

    😹 Update

---
+++ AtCoder/JOI/JOI2026YO1A/A.fsx
@@ -0,0 +1,16 @@
+// https://atcoder.jp/contests/joi2026yo1a/tasks/joi2026_yo1a_a
+
+let fn y b =
+    y * b * b
+
+printfn "%i" (fn 3 2)
+//=> 12
+
+printfn "%i" (fn 10 5)
+//=> 250
+
+printfn "%i" (fn 1 1)
+//=> 1
+
+printfn "%i" (fn 100 100)
+//=> 1000000
