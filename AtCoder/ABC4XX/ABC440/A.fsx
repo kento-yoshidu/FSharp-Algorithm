addefe0286c7ca9e29bc9b83c9db0a0aacdc5dd3
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-01-11 14:16:29 +0900

    ♟️ ABC440 A

---
+++ AtCoder/ABC4XX/ABC440/A.fsx
@@ -0,0 +1,13 @@
+// https://atcoder.jp/contests/abc440/tasks/abc440_a
+
+let fn x y =
+    x * pown 2 y
+
+printfn "%i" (fn 110 2)
+//=> 440
+
+printfn "%i" (fn 233 3)
+//=> 1864
+
+printfn "%i" (fn 432 1)
+//=> 864
