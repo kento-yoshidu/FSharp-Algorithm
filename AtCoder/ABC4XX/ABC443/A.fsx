b4f8e1c4894a286a6af969ac123ef9b4c216dd4d
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-02-01 12:41:04 +0900

    📉 ABC443 A

---
+++ AtCoder/ABC4XX/ABC443/A.fsx
@@ -0,0 +1,13 @@
+// https://atcoder.jp/contests/abc443/tasks/abc443_a
+
+let fn s =
+    s + "s"
+
+printfn "%s" (fn "http")
+//=> https
+
+printfn "%s" (fn "append")
+//=> appends
+
+printfn "%s" (fn "beginner")
+//=> beginners
