c46d4ecd6685921c43cd0ae511740b1d87cf08fc
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-05-10 12:41:37 +0900

    🫔 ABC231 A

---
+++ AtCoder/ABC2XX/abc231/a.fsx
@@ -0,0 +1,13 @@
+// https://atcoder.jp/contests/abc231/tasks/abc231_a
+
+let fn d =
+    d / 100.0
+
+printfn "%f" (fn 1000)
+//=> 10.000000
+
+printfn "%f" (fn 50)
+//=> 0.500000
+
+printfn "%f" (fn 3141)
+//=> 31.410000
